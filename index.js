ad03014388c429252a2492e42508a5306d15aa53
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2020-05-06 04:19:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,96 +1,52 @@
-// Loading our modules.
-const Discord = require('discord.js');
-const Cleverbot = require('cleverbot-node');
-var prefix = '$';
-// Setting up our clients.
-const client = new Discord.Client();
-const clbot = new Cleverbot();
-clbot.configure({botapi: 'CC52k4616pisXEsUkdqa9PkenmQ'});
+const CLEVERBOT_KEY = "CC52k4616pisXEsUkdqa9PkenmQ"
 
+const { Client } = require('discord.js');
+const request = require('node-superfetch');
+const winston = require('winston');
+const client = new Client({ disableEveryone: true });
+const blankResponses = ['What?', 'Huh?', 'I don\'t understand.'];
+const logger = winston.createLogger({
+	transports: [new winston.transports.Console()],
+	format: winston.format.combine(
+		winston.format.timestamp({ format: 'MM/DD/YYYY HH:mm:ss' }),
+		winston.format.printf(log => `[${log.timestamp}] [${log.level.toUpperCase()}]: ${log.message}`)
+	)
+});
+const convos = new Map();
+let mentionRegex;
 
-
-
-/*
-    This will make the bot reply when it gets mentioned.
-    (Thanks to GeopJr for the `message.content.includes` part!)
-*/
-client.on('message', (message) => {
-  if (message.content.includes(client.user.id)) {
-    clbot.write(message.content, (response) => {
-      message.channel.startTyping();
-      setTimeout(() => {
-        message.channel.send(response.output).catch(console.error);
-        message.channel.stopTyping();
-      }, Math.random() * (1 - 3) + 1 * 1000);
-    });
-  }
+client.on('ready', () => {
+	logger.info(`[READY] Logged in as ${client.user.tag}! ID: ${client.user.id}`);
+	mentionRegex = new RegExp(`<@!?${client.user.id}>`);
 });
 
-/*
-    As example, you have a channel named #ai.
-    Well, have a chat with the bot in there. No tagging.
-*/
-client.on('message', async (message) => {
-  if (message.author.id == config.bot_id) return;
-  if (message.channel.name == 'ai') {
-    clbot.write(message.content, (response) => {
-      message.channel.startTyping();
-      setTimeout(() => {
-        message.channel.send(response.output).catch(console.error);
-        message.channel.stopTyping();
-      }, Math.random() * (1 - 3) + 1 * 1000);
-    });
-  }
+client.on('message', async msg => {
+	if ((msg.channel.type === 'text' && !msg.mentions.has(client.user.id)) || msg.author.bot) return;
+	try {
+		const convo = convos.get(msg.channel.id);
+		const { body } = await request
+			.get('https://www.cleverbot.com/getreply')
+			.query({
+				key: CLEVERBOT_KEY,
+				cs: convo ? convo.cs : '',
+				input: msg.content.replace(mentionRegex, '').trim()
+			});
+		if (convo) clearTimeout(convo.timeout);
+		const timeout = setTimeout(() => convos.delete(msg.channel.id), 600000);
+		convos.set(msg.channel.id, { cs: body.cs, timeout });
+		await msg.reply(body.output || blankResponses[Math.floor(Math.random() * blankResponses.length)]);
+	} catch (err) {
+		await msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+	}
 });
 
-
-client.on('message', message => {
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== "623866539925307406") return;
-
-  
-  if (message.content.startsWith(prefix + 'setwatch')) {
-  client.user.setActivity(argresult, {type: 'WATCHING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Watch Now: **${argresult}**`)
-} 
-
- 
-  if (message.content.startsWith(prefix + 'setlis')) {
-  client.user.setActivity(argresult, {type: 'LISTENING'})
-     console.log('test' + argresult);
-    message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
-} 
-
-
-if (message.content.startsWith(prefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`Username Changed To **${argresult}**`)
-  return message.reply("You Can change the username 2 times per hour");
-} 
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
-}
-
-if (message.content.startsWith(prefix + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}**`)
-} 
-if (message.content.startsWith(prefix + 'setplay')) {
-  client.user.setGame(argresult);
-     console.log('test' + argresult);
-    message.channel.sendMessage(`Playing: **${argresult}**`)
-} 
-
-
-
+client.on('disconnect', event => {
+	logger.error(`[DISCONNECT] Disconnected with code ${event.code}.`);
+	process.exit(0);
 });
 
+client.on('error', err => logger.error(err));
 
-// Login go the bot.
-client.login(process.env.BOT_TOKEN);
+client.on('warn', warn => logger.warn(warn));
+
+client.login(process.env.BOT__TOKEN);
